937e4d4ff640a75bd6abd9e2d8e92796db3fd752
jerichen <jeri.chen0805@yahoo.com.tw>
2017-07-30 17:19:09 +0800

    fix run-ssl.sh

--- nginx/scripts/run-ssl.sh
+++ nginx/scripts/run-ssl.sh
@@ -9,15 +9,19 @@ if ! [ -z $site ]; then
     openssl req -nodes -new -x509 -keyout /etc/nginx/ssl/"$site".key -out /etc/nginx/ssl/"$site".crt -subj "/C=US/ST=NY/L=NYC/O=Dis/CN=$site"
 
     echo "--- Inserting SSL directives into site's server file. ---"
-    cp /etc/nginx/sites-available/default.conf /etc/nginx/sites-available/$site.conf
-    sed -i "s/localhost/dev.$site/g" /etc/nginx/sites-available/$site.conf
-    sed -i "s/\var/\www/\html/var/\www/\html/\$site/\public" /etc/nginx/sites-available/$site.conf
-    sed -i "/listen [::]:80;/a \\\n   ssl_certificate /etc/nginx/ssl/$site.crt;\n    ssl_certificate_key /etc/nginx/ssl/$site.key;\n\n"  /etc/nginx/sites-available/$site.conf
+    cp /etc/nginx/sites-available/default.conf /etc/nginx/sites-available/"$site".conf
+    sed -i "s/localhost/dev.$site/g" /etc/nginx/sites-available/"$site".conf
+    sed -i "s/\var/\www/\html/var/\www/\html/\$site/\public" /etc/nginx/sites-available/"$site".conf
+    sed -i "s/default.crt/$site.crt/g" /etc/nginx/sites-available/"$site".conf
+    sed -i "s/default.key/$site.key/g" /etc/nginx/sites-available/"$site".conf
+    #sed -i "/listen [::]:80;/a \\\n   ssl_certificate /etc/nginx/ssl/$site.crt;\n    ssl_certificate_key /etc/nginx/ssl/$site.key;\n\n"  /etc/nginx/sites-available/"$site".conf
 
-    cp /etc/nginx/sites-available/default_ssl.conf /etc/nginx/sites-available/$site_ssl.conf
-    sed -i "s/localhost/dev.$site/g" /etc/nginx/sites-available/$site_ssl.conf
-    sed -i "s/\var/\www/\html/var/\www/\html/\$site/\public" /etc/nginx/sites-available/$site_ssl.conf
-    sed -i "/listen [::]:443;/a \\\n   ssl_certificate /etc/nginx/ssl/$site.crt;\n    ssl_certificate_key /etc/nginx/ssl/$site.key;\n\n"  /etc/nginx/sites-available/$site_ssl.conf
+    cp /etc/nginx/sites-available/default_ssl.conf /etc/nginx/sites-available/"$site"_ssl.conf
+    sed -i "s/localhost/dev.$site/g" /etc/nginx/sites-available/"$site"_ssl.conf
+    sed -i "s/\var/\www/\html/var/\www/\html/\$site/\public" /etc/nginx/sites-available/"$site"_ssl.conf
+    sed -i "s/default.crt/$site.crt/g" /etc/nginx/sites-available/"$site"_ssl.conf
+    sed -i "s/default.key/$site.key/g" /etc/nginx/sites-available/"$site"_ssl.conf
+    #sed -i "/listen [::]:443;/a \\\n   ssl_certificate /etc/nginx/ssl/$site.crt;\n    ssl_certificate_key /etc/nginx/ssl/$site.key;\n\n"  /etc/nginx/sites-available/"$site"_ssl.conf
 else
 # does exist 80.443 port config
     echo "--- run-ssl error ---"
