3e74d803bbd0cafaee6e7613e78634a070130b6d
jerichen <jeri.chen0805@yahoo.com.tw>
2017-08-30 14:44:42 +0800

    remove fail file

--- nginx/scripts/hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Hello World!'
-
-
-echo 'alias exit="echo Are you sure? this will kill the container. use Ctrl + p, Ctrl + q to detach or ctrl + d to exit"' > ~/.bashrc
-bash
-
-
